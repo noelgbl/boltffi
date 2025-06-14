a62ca089ace0762993d0034f216fe6fd73f267f7
Ali <ali94_200970@yahoo.com>
2025-06-14 20:16:00 +0200

    fix header output to OUT_DIR

--- mobiFFI_core/build.rs
+++ mobiFFI_core/build.rs
@@ -1,12 +1,16 @@
+use std::path::PathBuf;
+
 fn main() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let header_path = out_dir.join("mobiFFI_core.h");
     
     cbindgen::Builder::new()
         .with_crate(&crate_dir)
         .with_config(cbindgen::Config::from_root_or_default(&crate_dir))
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("bindings.h");
+        .write_to_file(&header_path);
     
     println!("cargo:rerun-if-changed=src/");
 }
