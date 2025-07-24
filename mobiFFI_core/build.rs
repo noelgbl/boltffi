624e28902ac394267c373f06cea3766e6dc65278
Ali <ali94_200970@yahoo.com>
2025-07-25 00:52:00 +0200

    fix cbindgen for pure C header

--- mobiFFI_core/build.rs
+++ mobiFFI_core/build.rs
@@ -1,16 +1,25 @@
 use std::path::PathBuf;
 
 fn main() {
-    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let crate_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let workspace_root = crate_dir.parent().unwrap();
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
     let header_path = out_dir.join("mobiFFI_core.h");
     
+    let config_path = workspace_root.join("cbindgen.toml");
+    let config = if config_path.exists() {
+        cbindgen::Config::from_file(&config_path).unwrap()
+    } else {
+        cbindgen::Config::default()
+    };
+    
     cbindgen::Builder::new()
         .with_crate(&crate_dir)
-        .with_config(cbindgen::Config::from_root_or_default(&crate_dir))
+        .with_config(config)
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(&header_path);
     
     println!("cargo:rerun-if-changed=src/");
+    println!("cargo:rerun-if-changed=../cbindgen.toml");
 }
